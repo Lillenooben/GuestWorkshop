da5378653770734dd7517eb2d6fad2091118dfcd
Jesper Li <jesperli99@hotmail.com>
2022-11-07 08:08:19 +0100

    Initial Commit

---
+++ GuestWorkshop/GuestWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuestWorkshopApp.swift
+//  GuestWorkshop
+//
+//  Created by Jesper Li on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct GuestWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
